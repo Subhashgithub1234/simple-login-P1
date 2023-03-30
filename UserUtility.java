91dae4ce35a74ab0078b232dee46735d804a3500
Subhashgithub1234 <129413092+Subhashgithub1234@users.noreply.github.com>
2023-03-31 00:31:23 +0530

    Create UserUtility.java

---
+++ UserUtility.java
@@ -0,0 +1,9 @@
+package project1_mail;
+
+public interface UserUtility {
+	String DRIVER="com.mysql.cj.jdbc.Driver";
+	String URL="jdbc:mysql://localhost:3306/project001";
+	String USER="root";
+	String PASSWORD="Mysql@123";
+
+}
